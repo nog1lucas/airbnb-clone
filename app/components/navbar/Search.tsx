d86256f232f757ca8ced52b6a3fc4539d5753dd3
nog1lucas <lnogueira.contato@gmaiil.com>
2023-07-22 17:53:42 -0300

    update

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import React from 'react'
+
+const Search = () => {
+    return (
+        <div className='
+    border-[1px]
+    w-full
+    md:w-auto
+    py-2
+    rounded-full
+    shadow-sm
+    hover:shadow-md
+    transition
+    cursor-pointer
+    '
+        >
+            <div className="
+            text-sm
+            font-semibold
+            px-6
+            ">
+                Anywhere
+            </div>
+
+        </div>
+    )
+}
+
+export default Search
